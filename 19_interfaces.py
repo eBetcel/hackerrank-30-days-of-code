7771089bc49875f012618dc4e9187f92e97cef2f
Emanuel Betcel <emanuelbetcel@gmail.com>
2022-03-06 11:57:13 -0300

    add days 16, 17, 18 and 19

---
+++ 19_interfaces.py
@@ -0,0 +1,13 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        sum = 0
+        for i in range(1,n+1):
+            if n % i == 0:
+                sum += i
+        
+        return sum
+
